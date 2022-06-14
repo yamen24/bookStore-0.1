27807b1a49c90f49482de0a599bdda283c12eb74
Mokter123 <104098735+Mokter123@users.noreply.github.com>
2022-06-15 01:18:12 +0800

    Add files via upload
    Layout for dashboard, logging and registration

---
+++ logreg.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from coderthemes.com/moltran/layouts/blue-vertical/pages-login.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 04 Jul 2020 06:48:32 GMT -->
+<head>
+  <meta charset="utf-8" />
+  <title>@yield('page_title')</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta content="Responsive bootstrap 4 admin template" name="description" />
+  <meta content="Coderthemes" name="author" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- App favicon -->
+  <link rel="shortcut icon" href="{{asset('backend_assets')}}/images/favicon.ico">
+
+  <!-- App css -->
+  <link href="{{asset('backend_assets')}}/css/bootstrap.min.css" rel="stylesheet" type="text/css" id="bootstrap-stylesheet" />
+  <link href="{{asset('backend_assets')}}/css/icons.min.css" rel="stylesheet" type="text/css" />
+  <link href="{{asset('backend_assets')}}/css/app.min.css" rel="stylesheet" type="text/css" id="app-stylesheet" />
+
+</head>
+
+<body class="authentication-page">
+
+
+  @yield('logreg_content')
+
+  <!-- Vendor js -->
+  <script src="{{asset('backend_assets')}}/js/vendor.min.js"></script>
+  <!-- App js -->
+  <script src="{{asset('backend_assets')}}/js/app.min.js"></script>
+</body>
+<!-- Mirrored from coderthemes.com/moltran/layouts/blue-vertical/pages-login.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 04 Jul 2020 06:48:32 GMT -->
+</html>
